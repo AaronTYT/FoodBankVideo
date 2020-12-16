6f73d004d992ed199ef8ba76e22dc08229cfd38e
Aaron Tan <aaron.tanyutat@yahoo.com.au>
2020-12-16 17:52:01 +0800

    first commit not complete

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <h1>Videos</h1>
+
+        <h2>Know your Food Portions</h2>
+
+       
+        <div className="row">
+          <iframe className="col2-3 portionVideo" src="//www.youtube.com/embed/SAzOz_V5SKg"></iframe>
+        </div>
+        
+
+        <h2>Safety</h2>
+        <div className="row">
+          <iframe className="col1-3" src="https://www.youtube.com/embed/oUVCWNQFGTc"></iframe>
+          <iframe className="col1-3" src="https://www.youtube.com/embed/uPiCGs4E7qc"></iframe>
+          
+        </div>
+
+        <h2>Cutting</h2>
+        <div className="row">
+          <iframe className="col1-3" src="https://www.youtube.com/embed/-Y2Jbqgtigo"></iframe>
+          <iframe className="col1-3" src="https://www.youtube.com/embed/JNEo_h8aw4o"></iframe>
+          <iframe className="col1-3" src="https://www.youtube.com/embed/-SSs1OMo_9M"></iframe>
+        </div>
+
+        <footer>
+          <p>Copyright</p>
+        </footer>
+
+      </body>
     </div>
   );
 }
